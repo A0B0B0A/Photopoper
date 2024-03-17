0939d6062c2ad4a817ead43d6ea0a84b3c5ab21d
A0B0B0A <152081588+A0B0B0A@users.noreply.github.com>
2024-03-17 18:57:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from PIL import Image, ImageFilter
+from ui import Ui_Photopop
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QInputDialog
+
+class Widget(Ui_Photopop):
+    def   __init__(self):
+        super().__init__()  
+        self.ui = Ui_Photopop()
+        self.ui.setupUi(self)
+        
+app = QApplication([])
 
 
-with Image.open('beer.jpg') as original:
-    pic_gray= original.convert("L")
-    pic_gray.save("black.jpg")
-    pic_gray.show() 
+ex = Widget()
+ex.show()
+app.exec_()
+# with Image.open('beer.jpg') as original:
+#     pic_gray= original.convert("L")
+#     pic_gray.save("black.jpg")
+#     pic_gray.show() 
